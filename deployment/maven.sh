a62ef069495dcaa226b370854f6b20ac7c9d3ac0
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-06-21 14:44:47 +0800

    update server_deploy.sh

---
+++ deployment/maven.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_192
+export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}
--- deployment/server_deploy.sh
+++ deployment/server_deploy.sh
@@ -17,11 +17,7 @@ adapt() {
     wget https://www-us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -P /tmp
     sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
     sudo ln -s /opt/apache-maven-3.6.3 /opt/maven
-    sudo vim /etc/profile.d/maven.sh
-    export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_192
-    export M2_HOME=/opt/maven
-    export MAVEN_HOME=/opt/maven
-    export PATH=${M2_HOME}/bin:${PATH}
+    sudo cp maven.sh /etc/profile.d/maven.sh
     sudo chmod +x /etc/profile.d/maven.sh
     source /etc/profile.d/maven.sh
   fi
